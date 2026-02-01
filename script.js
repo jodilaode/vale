63e7cfe370b42e043633650148fb7754a33f7350
Jo <86167416+jodilaode@users.noreply.github.com>
2026-02-01 15:24:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,23 @@ const yesBtn = document.getElementById("yes");
 const noBtn = document.getElementById("no");
 const result = document.getElementById("result");
 const gif = document.getElementById("gif");
+const photo = document.getElementById("photo");
 
 yesBtn.onclick = () => {
-    result.innerHTML = "YAY! 🎉💖";
+    result.innerHTML = "YAY!!! 🎉💖";
     gif.style.display = "block";
+    photo.style.display = "block";
+
+    // Hilangin tombol
+    yesBtn.style.display = "none";
+    noBtn.style.display = "none";
 };
 
 noBtn.onmouseover = () => {
+    const x = Math.random() * 70;
+    const y = Math.random() * 70;
+
     noBtn.style.position = "absolute";
-    noBtn.style.top = Math.random() * 80 + "%";
-    noBtn.style.left = Math.random() * 80 + "%";
+    noBtn.style.left = x + "%";
+    noBtn.style.top = y + "%";
 };
